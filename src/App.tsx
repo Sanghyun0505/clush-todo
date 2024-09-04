4066184eca9ded4374009c7b4035432f3ca5167b
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-09-04 14:44:16 +0900

    feat: 라우팅 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Provider from "./components/Provider";
+import Router from "./router";
+import "./styles/font.css";
 
 const App = () => {
-  return <div>clush-todo</div>;
+  return (
+    <Provider>
+      <Router />
+    </Provider>
+  );
 };
 
 export default App;
